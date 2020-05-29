ce4b2bc2d00b7b2e06d72b3e347376d464494ca1
RobnettSean22 <robnettsean22@gmail.com>
2020-05-29 14:47:05 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import JedStreet from "./assests/JedStreet.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <img src={JedStreet} />
+      <div className='fire'></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
