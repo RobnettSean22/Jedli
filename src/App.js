c41d18f50060b9bd36d0c1de234982f8bfe4b957
RobnettSean22 <robnettsean22@gmail.com>
2020-05-30 01:34:32 +0900

    aligning sections for title placement

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import "./App.css";
 function App() {
   return (
     <div className='App'>
-      <div className='fire'></div>
-      <div></div>
-      <div></div>
+      <div className='first'></div>
+      <div className='mid'></div>
+      <div className='third'></div>
     </div>
   );
 }
