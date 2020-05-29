96a8666bbded0e4298c150765421e33f720c744b
RobnettSean22 <robnettsean22@gmail.com>
2020-05-30 00:47:46 +0900

    aligne image right need a new image'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import JedStreet from "./assests/JedStreet.jpg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
-      <img src={JedStreet} />
       <div className='fire'></div>
       <div></div>
       <div></div>
