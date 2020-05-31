e3fe5e4e6d8282ebafdf48cf0b056e9af2b387dd
RobnettSean22 <robnettsean22@gmail.com>
2020-05-31 19:36:43 +0900

    added three link titles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className='App'>
       <div className='head'></div>
       <div className='title'>
-        <h1>JedLi</h1>
+        <div>
+          <h1>JedLi</h1>
+        </div>
+        <div className='links'>
+          <ul>
+            <li>
+              <button>music</button>
+            </li>
+            <li>events</li>
+            <li>style</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
