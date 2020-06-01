d95e8d3ce12fee725ad0870adfc76c3e716eae9e
RobnettSean22 <robnettsean22@gmail.com>
2020-06-02 00:21:55 +0900

    laying the ground work for each page and it elements, in relation to the xd design

--- src/App.js
+++ src/App.js
@@ -24,8 +24,17 @@ function App() {
       <div id='music'>
         <div id='albums'></div>
       </div>
-      <div id='events'></div>
-      <div id='previous_events'></div>
+      <div id='events'>
+        <div id='timer'>
+          <div id='clock'></div>
+          <div id='current-event'></div>
+        </div>
+        <div id='directions'></div>
+      </div>
+      <div id='previous_events'>
+        <div id='event-links'></div>
+        <div id='past_events'></div>
+      </div>
       <div id='style'></div>
     </div>
   );
