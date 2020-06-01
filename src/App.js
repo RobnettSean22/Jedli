fa45d8f94d915ea53589ae8917e35840ac5fcaa8
RobnettSean22 <robnettsean22@gmail.com>
2020-06-01 20:33:30 +0900

    allow on scrolls for y acess

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import "./App.scss";
 function App() {
   return (
     <div className='App'>
-      <div className='head'></div>
-      <div className='title'>
-        <div>
-          <h1>JedLi</h1>
-        </div>
-        <div className='links'>
-          <ul>
-            <li>
-              <button>music</button>
-            </li>
-            <li>events</li>
-            <li>style</li>
-          </ul>
+      <div id='landing'>
+        <div className='head'></div>
+        <div className='title'>
+          <div>
+            <h1>JedLi</h1>
+          </div>
+          <div className='links'>
+            <ul>
+              <li>
+                <button>music</button>
+              </li>
+              <li>events</li>
+              <li>style</li>
+            </ul>
+          </div>
         </div>
       </div>
+      <div id='albums'>hello</div>
     </div>
   );
 }
