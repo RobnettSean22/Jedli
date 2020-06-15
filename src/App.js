267028f757c116abf6ad5feddeb1685f7d90f8b2
RobnettSean22 <robnettsean22@gmail.com>
2020-06-15 23:18:46 +0900

    change everything design wise utilizing react springs as well

--- src/App.js
+++ src/App.js
@@ -1,69 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Switch, Route, __RouterContext } from "react-dom";
+import { useTransition, animated } from "react-spring";
+import Music from "./compnents/Music";
 import "./App.scss";
 
-function App() {
-  return (
-    <div className='App'>
-      <div id='landing'>
-        <div className='head'></div>
-        <div className='title'>
-          <div>
-            <h1>JedLi</h1>
-          </div>
-          <div className='links'>
-            <ul>
-              <li>
-                <a href='#music'>
-                  <button>music</button>
-                </a>
-              </li>
-              <li>
-                <a href='#events'>events</a>
-              </li>
-              <li>
-                <a href='#styles'>styles</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+const App = () => {
+  const { location } = useContext(__RouterContext);
+  const transitions = useTransition(location, location => location.pathname, {
+    from: { opacity: 0, transform: "translate(0%,100%" },
+    enter: { opacity: 1, transform: "translate(0,0%" },
+    leave: { opacity: 0, transform: "translate(0,-50%" }
+  });
+
+  return transitions.map(({ item, props, key }) => (
+    <div>
+      <div id='tiled'>
+        <animated.div key={key} style={props}>
+          <Switch location={item}>
+            <Route exact path='/' component={Music} />
+          </Switch>
+        </animated.div>
       </div>
-      <div id='music'>
-        <div id='albums'></div>
-      </div>
-      <div id='events'>
-        <div id='timer'>
-          <div id='clock'>
-            <div id='clock_container'></div>
-          </div>
-          <div id='current-event'>
-            <div>
-              <h1>Event title</h1>
-            </div>
-            <div>
-              <h2>Loocations</h2>
-            </div>
-            <div>
-              <h3>Date</h3>
-            </div>
-          </div>
-        </div>
-        <div id='directions'>
-          <div>
-            <div className='event-info'></div>
-            <div className='event-info'></div>
-            <div className='event-info'></div>
-            <div className='event-info'></div>
-            <div className='event-info'></div>
-          </div>
-        </div>
-      </div>
-      <div id='previous_events'>
-        <div id='event-links'></div>
-        <div id='past_events'></div>
-      </div>
-      <div id='styles'></div>
     </div>
-  );
-}
+  ));
+};
 
 export default App;
