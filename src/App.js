3885d8f768ca1b670d30cf9d2f2628f273a55643
RobnettSean22 <robnettsean22@gmail.com>
2020-06-19 14:55:49 +0900

    setting up api calls from itunes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router";
-
+import axios from "axios";
 import Music from "./compnents/Music/Music";
 import Lyrics from "./compnents/Lyrics/Lyrics";
 import Events from "./compnents/Events/Events";
@@ -8,20 +8,44 @@ import PastEvents from "./compnents/PastEvents/PastEvents";
 import Story from "./compnents/Story/Story";
 import "./App.scss";
 
-const App = () => {
-  return (
-    <div>
-      <div id='tiled'>
-        <Switch>
-          <Route exact path='/' component={Music} />
-          <Route exact path='/lyrics' component={Lyrics} />
-          <Route exact path='/events' component={Events} />
-          <Route exact path='/past-events' component={PastEvents} />
-          <Route exact path='/story' component={Story} />
-        </Switch>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: ""
+    };
+  }
+
+  arival = () => {
+    axios
+      .get(
+        `https://itunes.apple.com/search?term=jedli
+    `
+      )
+      .then(res => {
+        this.state({
+          content: res.data
+        });
+      });
+  };
+
+  render() {
+    console.log(this.state.content);
+    return (
+      <div>
+        <div id='tiled'>
+          <Switch>
+            <Route exact path='/' exact render={() => <Music />} />
+            <Route exact path='/lyrics' component={Lyrics} />
+            <Route exact path='/events' component={Events} />
+            <Route exact path='/past-events' component={PastEvents} />
+            <Route exact path='/story' component={Story} />
+          </Switch>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
