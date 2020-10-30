b81e0927da8ed0faf1f4103898e055504ee2325a
RobnettSean22 <robnettsean22@gmail.com>
2020-10-30 15:14:08 +0900

    addtiltlist component to albumsonglist componet for title card build

--- src/compnents/ALbumSongList.jsx
+++ src/compnents/ALbumSongList.jsx
@@ -1,23 +1,34 @@
 import React, { useState } from "react";
+import TitleList from "./TitleList";
 import Header from "./Header";
+import Footer from "./Footer";
 import Stars from "../assests/stary_night.jpg";
 import styled from "styled-components";
 
 const Background = styled.div`
   width: 100vw;
   height: 100vh;
-  display: flex;
-  flex-direction: column;
+
   background-image: url(${Stars});
   background-repeat: no-repeat;
   background-size: cover;
 `;
 const Shadow = styled.div`
-width: 100%;
+    width: 100%;
     height: 100%;
+    display:flex;
+    flex-direction:column;
     background: linear-gradient( 0deg, rgba(66,32,3,0.57) 0%, rgba(16,14,13,0.74) 100% );
 }
 `;
+
+const TitleListGrid = styled.div`
+  display: grid;
+  grid-template-columns: 300px 300px;
+  grid-template-rows: 150px 150px 150px 150px 150px;
+  gap: 0px 0px;
+`;
+
 const Albumsonglist = props => {
   const [thumbnail, setThumbnail] = useState(
     props.location.state.singleAlbumArt
@@ -25,16 +36,20 @@ const Albumsonglist = props => {
   const [musicData, setMusicData] = useState(
     props.location.state.allJedliMusic
   );
-
+  const [singleAlbumTitles, setSingleAlbumTitles] = useState([]);
   const filtered = musicData.filter(items => {
     return items.collectionId === thumbnail.collectionId;
   });
+  setSingleAlbumTitles(filtered);
+
   console.log(filtered);
   console.log(props.location.state.allJedliMusic);
   return (
     <Background>
       <Shadow>
         <Header />
+        <TitleListGrid>{}</TitleListGrid>
+        <Footer />
       </Shadow>
     </Background>
   );
